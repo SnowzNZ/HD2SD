1bab114d75727efeb376de97322eca51091b08e4
SnowzNZ <jackson.baylis@outlook.co.nz>
2023-05-08 21:16:35 +1200

    fix logic and add more comments

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ Create SD images for osu! skins by downscaling HD images by 50%
 import glob
 import os
 import textwrap
+import time
 from tkinter import filedialog
 
 import readchar
@@ -30,6 +31,7 @@ if os.path.exists(default_skin_path):
 else:
     folder_path = filedialog.askdirectory()
 
+# Ask the user if they wish to continue
 option, _ = pick(
     ["Yes", "No"],
     textwrap.fill(f"Selected Folder: {folder_path}")
@@ -37,11 +39,14 @@ option, _ = pick(
 )
 
 if option == "Yes":
+    # Get start time of process
+    start_time = time.time()
+
     # Get every image in the folder
     for image in tqdm(
         glob.glob(rf"{folder_path}/*@2x.png"),
-        desc="Downscaling images",
-        unit=" images",
+        desc="Converting images to SD",
+        unit=" image",
     ):
         # Open the HD image
         hd_image = Image.open(image)
@@ -49,15 +54,25 @@ if option == "Yes":
         # Get the width and height of the image
         width, height = hd_image.size
 
-        if width >= 2 and height >= 2:
+        # Check if the image is more than 1x1, if not break out of the for loop as 1/2 != a whole number
+        if width > 1 and height > 1:
             size = (width // 2, height // 2)
         else:
-            size = (width, height)
+            break
 
         # Resize the image
         hd_image = hd_image.resize(size, Image.Resampling.LANCZOS)
+        # Save the image as an SD image
         hd_image.save(image.replace("@2x", ""))
 
-if option == "No":
-    print("Press any key to exit...")
-    key = readchar.readkey()
+    # Get end time of process
+    end_time = time.time()
+
+    # Calculate total time it took
+    elapsed_time = end_time - start_time
+
+    # Print total time it took to complete
+    print(f"\nCompleted in {round(elapsed_time, 3)} seconds.")
+
+print("Press any key to exit...")
+key = readchar.readkey()
